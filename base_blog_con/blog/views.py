8caf789730ee301a89cf0065bda1e82c59d743b4
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-11 21:36:04 +0300

    #Created templates file and first template

--- base_blog_con/blog/views.py
+++ base_blog_con/blog/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-#First view
 
-def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+def BlogHome(request):
+    return render(request, "blog/home.html")
