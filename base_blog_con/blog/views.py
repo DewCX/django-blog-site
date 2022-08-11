900af742b245375bcdace61e213dbb08d8027647
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-11 21:27:26 +0300

    #Created first views and url connection

--- base_blog_con/blog/views.py
+++ base_blog_con/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#First view
+
+def home(request):
+    return HttpResponse("<h1>Blog Home</h1>")
