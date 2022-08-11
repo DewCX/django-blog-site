fdd1a6627a592615335c6296839531553dd5242b
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-11 23:09:14 +0300

    #Created new app 'account'
    #Created new form in new app called forms.py
    #Created register.html

--- base_blog_con/base_blog/settings.py
+++ base_blog_con/base_blog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # My Apps
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
--- base_blog_con/base_blog/urls.py
+++ base_blog_con/base_blog/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from account import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
+
+    #User authentication
+    path('register/', account_views.register, name='register'),
 ]
+
+
+
