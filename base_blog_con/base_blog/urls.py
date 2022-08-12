bb14460f017eff89df7c5802f46405f4c4228cc6
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-12 21:58:30 +0300

    #Uploaded image into our profile

--- base_blog_con/base_blog/urls.py
+++ base_blog_con/base_blog/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from account import views as account_views
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
@@ -13,6 +16,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
