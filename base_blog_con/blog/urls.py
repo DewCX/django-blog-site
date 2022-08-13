3dc5983ed8e03179345773966cab6afc92f06b55
Cem Mandraci <cemmandraci@hotmail.com>
2022-08-13 10:06:52 +0300

    #Working with class based views
    #Updated  blog views.py, urls.py
    #Ordering posts according to new to old

--- base_blog_con/blog/urls.py
+++ base_blog_con/blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+#Working with Class based views
+from .views import PostListView
+
 urlpatterns = [
-    path("", views.BlogHome, name="blog-home"),
-    path("about/", views.BlogAbout, name="blog-about")
+    # path("", views.BlogHome, name="blog-home"),
+    path("about/", views.BlogAbout, name="blog-about"),
+
+    #Working with class based views
+    path("", PostListView.as_view(), name="blog-home"),
 ]
 
--- base_blog_con/blog/views.py
+++ base_blog_con/blog/views.py
@@ -2,13 +2,26 @@ from multiprocessing import context
 from django.shortcuts import render
 from .models import Post
 
+#Working with class based views
+from django.views.generic import ListView
 
-def BlogHome(request):
-    context = {
-        "posts":Post.objects.all()
-    }
-    return render(request, "blog/home.html",context)
+
+# def BlogHome(request):
+#     context = {
+#         "posts":Post.objects.all()
+#     }
+#     return render(request, "blog/home.html",context)
 
 
 def BlogAbout(request):
     return render(request, "blog/about.html")
+
+
+
+#Class Based Views
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
